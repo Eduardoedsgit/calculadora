87148713f8c2d1cf25501eb2e0e197d6b54d9dc9
Eduardoedsgit <142338989+Eduardoedsgit@users.noreply.github.com>
2023-08-15 15:25:52 -0300

    Create calculadora.py
    uma bela atualização completando as operações da calculadora.

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+
+#calculadora básica
+
+num1 = float(input('Digite um número: '));
+num2 = float(input("Diite o outro número: "));
+oper = str(input('+ , -, x, / Digite um símbolo para a operação desejada: '))
+print("operação digitada é : {}".format(oper));
+
+if(oper == '+'):
+    soma = (num1 + num2);
+    print('{} + {} = {}'.format(num1, num2 ,soma));
+if(oper == '-'):
+    sub = (num1 - num2);
+    print("{} - {} = {}".format(num1, num2, sub));
+if(oper == 'x'):
+    multi = (num1 * num2);
+    print("{} x {} = {}".format(num1, num2, multi));
+if(oper == '/'):
+    divi = (num1 / num2);
+    print("{} / {} = {}".format(num1, num2, divi));
